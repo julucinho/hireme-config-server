de99c27f6aeb51ed6a15ff07be37a89011de6292
Julucin <joselucioalmeidajunior@gmail.com>
2022-01-10 20:41:29 -0300

    feat: starting project

---
+++ src/test/java/com/capitolio/hiremeconfigserver/HiremeConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capitolio.hiremeconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiremeConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
